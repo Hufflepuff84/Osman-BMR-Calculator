b0ef981d736cdbd73ca18b8c758936bc10f13a7b
Osman Samad <osamad84@yahoo.com>
2020-06-01 18:48:39 -0500

    more changes

--- ClientApp/src/app/counter/counter.component.ts
+++ ClientApp/src/app/counter/counter.component.ts
@@ -1,52 +1,19 @@
-
-import { Component, Inject, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-counter',
-    templateUrl: './counter.component.html'
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.sass']
 })
-export class CounterComponent implements OnInit {
-    public persons: Person[];
-    public newPerson: Person = {Gender:'',Age:0, Weight:0, Height:0};
+export class CounterComponent {
+    title = 'My BMR Calculator';
+    public num1: number;
+    public num2: number;
+    public num3: number;
+    public num4: number;
 
-    public bmr: number;
-
-
-    constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) {
-
+    calculate(){
+        this.num4 = this.num1 + this.num2 + this.num3;
     }
 
-    async ngOnInit() {
-        
-
-        //this.persons = await this.http.get<Person[]>(this.baseUrl + 'person').toPromise();
-    }
-    //async save() {
-       // await this.http.post<Person[]>(this.baseUrl + 'person', this.newPerson).toPromise();
-        //this.newPerson = {Age:0, Weight:0, Height:0};
-        //this.persons = await this.http.get<Person[]>(this.baseUrl + 'person').toPromise();
-    //}
-
-
-}
-
-//console.log ("Please enter your gender");
-//var gender = console.log("Please enter your gender");
-//console.log ("Please enter your weight in kilograms");
-//var weight = console.log ("Please enter your weight in kilograms");
-//console.log ("Please enter your height in centimeters");
-//var height = console.log ("Please enter your height in centimeters");
-//console.log ("Please enter your age");
-//var age = console.log ("Please enter your age");
-
-
-// var bmr = 66 + (13.8 * Weight)  + (5 * height) - (6.8 * age);
-
-interface Person {
-  //Id: number;
-  Gender: string;
-  Age: number;
-  Weight: number;
-  Height: number;
 }
