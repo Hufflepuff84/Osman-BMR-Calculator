4683aefb28828fc81cb6c0c01cce75efa6e6e962
Osman Samad <osamad84@yahoo.com>
2020-06-14 14:45:53 -0500

    more changes

--- ClientApp/src/app/person/person.component.ts
+++ ClientApp/src/app/person/person.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+
+import { Component, Inject, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-person',
-  templateUrl: './person.component.html',
-  styleUrls: ['./person.component.css']
+    selector: 'app-person',
+    templateUrl: './person.component.html'
 })
 export class PersonComponent implements OnInit {
+    public persons: Person[];
+    public newPerson: Person = {firstName:'', lastName: ''};
 
-  constructor() { }
+    constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) {
 
-  ngOnInit() {
-  }
+    }
+    async ngOnInit() {
+        this.persons = await this.http.get<Person[]>(this.baseUrl + 'person').toPromise();
+    }
+    async save() {
+        await this.http.post<Person[]>(this.baseUrl + 'person', this.newPerson).toPromise();
+        this.newPerson = { firstName: '', lastName: '' };
+        this.persons = await this.http.get<Person[]>(this.baseUrl + 'person').toPromise();
+    }
 
+
+}
+
+interface Person {
+    firstName: string;
+    lastName: string;
 }
