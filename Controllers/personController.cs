ee6d8acea1e4d78663c4ef97ade041217f668cce
jordan.woodruff <jordan.woodruff@tylertech.com>
2020-06-17 19:19:20 -0500

    finish controller

--- Controllers/personController.cs
+++ Controllers/personController.cs
@@ -1,30 +1,49 @@
-// using System;
-// using System.Collections.Generic;
-// using System.Linq;
-// using System.Threading.Tasks;
-// using capstone.Data;
-// using capstone.Models;
-// using Microsoft.AspNetCore.Authorization;
-// using Microsoft.AspNetCore.Mvc;
-// using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using capstone.Data;
+using capstone.Models;
+using Microsoft.AspNetCore.Mvc;
 
-// namespace capstone.Controllers
-// {
-//     [Authorize]
-//     [ApiController]
-//     [Route("[controller]")]
-//     public class personController : ControllerBase
-//     {
-//         [HttpGet]
-//         public IEnumerable<Person> Get()
-//         {
-//             Person[] person = null;
-//             using (var context = new ApplicationDbContext())
-//             {
-//                 person =  context.Persons.ToArray();
-//             }
-//             return person;
+namespace capstone.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PersonController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Person> Get([FromQuery]string userId)
+        {
+            if(string.IsNullOrEmpty(userId))
+                userId = HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            return _context.Persons.Where(p => p.userId == userId).ToArray(); 
+        }
+
+        [Route("all")]
+        [HttpGet]
+        public IEnumerable<Person> Get()
+        {
+            return _context.Persons;
+        }
+
+        [HttpPost]
+        public Person Post([FromBody]Person person)
+        {
+            if(string.IsNullOrEmpty(person.userId))
+                person.userId = HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
             
-//         }
-//     }
-// }
+            _context.Persons.Add(person);
+            _context.SaveChanges();
+
+            return person;
+        }
+    }
+}
