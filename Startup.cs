35cb9cb6c935103592e40a8255d19b659fc84b65
jordan.woodruff <jordan.woodruff@tylertech.com>
2020-06-20 15:31:26 -0500

    startup debug

--- Startup.cs
+++ Startup.cs
@@ -51,18 +51,9 @@ namespace capstone
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseDatabaseErrorPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                //app.UseHsts();
-            }
 
+            app.UseDeveloperExceptionPage();
+            app.UseDatabaseErrorPage();
             //app.UseHttpsRedirection();
             app.UseStaticFiles();
             if (!env.IsDevelopment())
